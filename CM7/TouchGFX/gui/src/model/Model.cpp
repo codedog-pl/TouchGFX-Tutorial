359e80acdc67d687c8e3f342e6e3858e36a70388
Adam Lyskawa <adam@codedog.pl>
2022-03-31 02:11:29 +0200

    ADD: HAL binding.

--- CM7/TouchGFX/gui/src/model/Model.cpp
+++ CM7/TouchGFX/gui/src/model/Model.cpp
@@ -8,6 +8,8 @@ Model::Model() : modelListener(0)
 
 void Model::tick()
 {
+	bool testPinState = BRIDGE_GetTestPinState();
+	if (testPinState != lastTestPinState) modelListener->testPinStateChanged(lastTestPinState = testPinState);
 	frame++;
 	if ((frame % 60) == 0) {
 		second++;
